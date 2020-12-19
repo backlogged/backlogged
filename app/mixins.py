1f926d80aced93b3a9f26f72a67d7396ec29cf11
backlogged-project <code@backlogged.games>
2020-12-18 23:35:42 -0500

    made heroku redirect permanent

--- app/mixins.py
+++ app/mixins.py
@@ -7,6 +7,6 @@ class HerokuRedirectMixin:
         absolute_uri = request.build_absolute_uri()
         if heroku_url in absolute_uri:
             redirect_url = absolute_uri.replace(heroku_url, "backlogged.games")
-            return redirect(redirect_url)
+            return redirect(redirect_url, permanent=True)
 
         return super().dispatch(request, *args, **kwargs)
